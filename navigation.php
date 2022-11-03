7c0f7ae05ce6ab982aec825dc6703e5f8079ff54
samuel <samijemberu@gmail.com>
2022-11-03 10:57:30 +0300

    n

---
+++ navigation.php
@@ -0,0 +1,64 @@
+<div class="" style="background-color:#fcb913;padding:0;">
+    <div class="row gx-0 align-items-center">
+        <div class="col-lg-7 px-5 text-start">
+            <div class="h-100 d-inline-flex align-items-center me-4">
+                <small class="fa fa-phone-alt me-2"></small>
+                <small>+012 345 6789</small>
+            </div>
+            <!-- <div class="h-100 d-inline-flex align-items-center me-4">
+                <small class="far fa-envelope-open me-2"></small>
+                <small>info@example.com</small>
+            </div> -->
+        </div>
+
+    </div>
+</div>
+<!-- Topbar End -->
+
+
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top px-4 px-lg-5">
+    <a href="index.html" class="navbar-brand d-flex align-items-center">
+        <h2 class="m-0" style="color:#fcb913; "><img class="img-fluid me-3" src="img/icon/mer.png" alt="">MERKEZA HONEY</h2>
+    </a>
+    <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav mx-auto bg-light pe-4 py-3 py-lg-0">
+            <a href="index.html" class="nav-item nav-link active" style="color:#154700;">Home</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">About</a>
+                <div class="dropdown-menu bg-light border-0 m-0">
+                    <a href="whoweare.html" class="dropdown-item">who we are</a>
+                    <a href="theethiopianhoney.html" class="dropdown-item">The Ethiopian Honey</a>
+                    <a href="whychooseus.html" class="dropdown-item">Why choose us</a>
+
+                </div>
+            </div>
+
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Products</a>
+                <div class="dropdown-menu bg-light border-0 m-0">
+                    <a href="myh.html" class="dropdown-item">Merkeza Yellow Honey </a>
+                    <a href="mch.html" class="dropdown-item">Merkeza Coffee Honey </a>
+                    <a href="mfh.html" class="dropdown-item">Merkeza Forest Honey </a>
+                    <a href="myh.html" class="dropdown-item">Merkeza White Honey </a>
+                    <a href="bfmh.html" class="dropdown-item">Blended Flower Merkeza Honey </a>
+                </div>
+            </div>
+            <a href="news.html" class="nav-item nav-link">news</a>
+            <a href="contact.html" class="nav-item nav-link">Contact us</a>
+
+        </div>
+
+
+        <div class="h-100 d-lg-inline-flex align-items-center d-none">
+            <a class="btn btn-square rounded-circle bg-light text-success me-2" href=""><i class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-square rounded-circle bg-light text-success me-2" href=""><i class="fab fa-twitter"></i></a>
+            <a class="btn btn-square rounded-circle bg-light text-success me-2" href=""><i class="fab fa-linkedin-in"></i></a>
+            <a class="btn btn-square rounded-circle bg-light text-success me-0" href=""><i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+</nav>
+<!-- Navbar End -->
